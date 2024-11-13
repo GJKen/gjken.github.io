dfa2cac410057ced6ae93034544715c9450f7a01
GJKen <gjkenryan@gmail.com>
2024-11-14 02:40:38 +0800

    update 2024/11/14  2:40

---
+++ static/ArticleCss.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', () => { document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css' })); Fancybox.bind('[data-fancybox="gallery"]', {}); });
